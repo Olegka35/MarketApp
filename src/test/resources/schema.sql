bc2e80f0bbdd5b1e4ba855a252a9bcb40f86166b
Oleg <olegka35@gmail.com>
2025-11-07 13:22:21 +0300

    Test Data generation

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -73,3 +73,12 @@ INSERT INTO offerings (title, description, img_path, price) VALUES
     ('Рюкзак городской', 'Лёгкий водоотталкивающий рюкзак с отделением для ноутбука 15.6"', '/backpack.jpg', 2790),
     ('Настольная лампа', 'Светодиодная лампа с регулировкой яркости и тёплого/холодного света', '/desk_lamp.jpg', 1590),
     ('Зонт складной', 'Компактный автоматический зонт, устойчивый к ветру', '/umbrella.jpg', 1190);
+
+INSERT INTO cart (offering_id, amount) VALUES (2, 2),
+                                              (5, 1);
+
+INSERT INTO orders (total_price) VALUES (1000);
+
+INSERT INTO order_items (order_id, offering_id, amount)
+VALUES (1, 2, 2, 400),
+       (1, 2, 1, 200);
