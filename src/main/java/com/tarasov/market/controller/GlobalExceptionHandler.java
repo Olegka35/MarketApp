012bf2dd0b7b07fc10569bb451789c330dbb9fb4
Oleg <olegka35@gmail.com>
2025-11-25 15:14:56 +0300

    Global Exception Handler refactoring

--- src/main/java/com/tarasov/market/controller/GlobalExceptionHandler.java
+++ src/main/java/com/tarasov/market/controller/GlobalExceptionHandler.java
@@ -1,19 +1,26 @@
 package com.tarasov.market.controller;
 
 
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.NoResultException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import reactor.core.publisher.Mono;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler( { EntityNotFoundException.class, NoResultException.class })
+    @ExceptionHandler( { NoSuchElementException.class, IllegalStateException.class })
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundEntityException() {
-        return "error";
+    public Mono<String> handleNotFoundEntityException() {
+        return Mono.just("error");
+    }
+
+    @ExceptionHandler( { Exception.class })
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Mono<String> handleInternalErrorException() {
+        return Mono.just("error");
     }
 }
