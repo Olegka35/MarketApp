b87275b6d2401bf1e39cf16ee46402b252486926
Oleg <olegka35@gmail.com>
2025-12-09 23:05:05 +0300

    Error response bodies

--- payment-service/src/main/java/com/tarasov/payment/controller/GlobalExceptionHandler.java
+++ payment-service/src/main/java/com/tarasov/payment/controller/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.tarasov.payment.controller;
 
 import com.tarasov.payment.model.UnsufficientBalanceException;
+import com.tarasov.payment.model.dto.ErrorResponse;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,13 +24,13 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler(value = ConstraintViolationException.class, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Mono<String> handleValidationFailedException(Exception e) {
-        return Mono.just(e.getMessage());
+    public Mono<ErrorResponse> handleValidationFailedException(Exception e) {
+        return Mono.just(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(WebExchangeBindException.class)
+    @ExceptionHandler(value = WebExchangeBindException.class, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, List<String>>> handleWebExchangeBindException(WebExchangeBindException exception) {
         Map<String, List<String>> validationErrors = exception.getFieldErrors().stream()
                 .collect(Collectors.groupingBy(
@@ -43,15 +45,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(validationErrors);
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler(value = NoSuchElementException.class, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Mono<String> handleNonExistingElementException(Exception e) {
-        return Mono.just(e.getMessage());
+    public Mono<ErrorResponse> handleNonExistingElementException(Exception e) {
+        return Mono.just(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(UnsufficientBalanceException.class)
+    @ExceptionHandler(value = UnsufficientBalanceException.class, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.PAYMENT_REQUIRED)
-    public Mono<String> handleUnsufficientBalanceException(Exception e) {
-        return Mono.just(e.getMessage());
+    public Mono<ErrorResponse> handleUnsufficientBalanceException(Exception e) {
+        return Mono.just(new ErrorResponse(e.getMessage()));
     }
 }
