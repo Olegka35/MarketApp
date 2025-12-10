6bcb1f4098a2525627447e67bc2cd8a7c88e1bab
Oleg <olegka35@gmail.com>
2025-12-10 21:14:46 +0300

    OpenAPI controller and docs generation

--- payment-service/src/main/java/com/tarasov/payment/controller/AccountController.java
+++ payment-service/src/main/java/com/tarasov/payment/controller/AccountController.java
@@ -1,34 +1,37 @@
 package com.tarasov.payment.controller;
 
-
-import com.tarasov.payment.model.dto.BalanceDto;
-import com.tarasov.payment.model.dto.PaymentRequest;
+import com.tarasov.payment.model.BalanceInfo;
+import com.tarasov.payment.model.PaymentRequest;
 import com.tarasov.payment.service.AccountService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
 
 @RestController
 @RequiredArgsConstructor
 @Validated
-public class AccountController {
+public class AccountController implements AccountApi {
 
     private final AccountService accountService;
 
-    @GetMapping("/account/{id}")
-    public Mono<BalanceDto> getAccountBalance(@PathVariable @Positive Long id) {
+    @Override
+    public Mono<ResponseEntity<BalanceInfo>> getAccountById(Long id,
+                                                            ServerWebExchange exchange) {
         return accountService.getAccountBalance(id)
-                .map(BalanceDto::new);
+                .map(BalanceInfo::new)
+                .map(ResponseEntity::ok);
     }
 
-    @PostMapping("/account/{id}/payment")
-    public Mono<BalanceDto> makePayment(@PathVariable @Positive Long id,
-                                        @RequestBody @Valid PaymentRequest paymentRequest) {
-        return accountService.deductBalance(id, paymentRequest.amount())
-                .map(BalanceDto::new);
+    @Override
+    public Mono<ResponseEntity<BalanceInfo>> makePayment(Long id,
+                                                         Mono<PaymentRequest> paymentRequest,
+                                                         ServerWebExchange exchange) {
+        return paymentRequest
+                .flatMap(request -> accountService.deductBalance(id, request.getAmount()))
+                .map(BalanceInfo::new)
+                .map(ResponseEntity::ok);
     }
 }
